6cc1d21d410bb4c96fe3ce6fc2057a7f144b0f75
AtidSpikes <AtidSpikes@192.168.133.115>
2014-11-02 17:46:52 +0200

    Add digital input in Arm

--- src/edu/wpi/first/wpilibj/templates/RobotMap.java
+++ src/edu/wpi/first/wpilibj/templates/RobotMap.java
@@ -6,19 +6,24 @@
 package edu.wpi.first.wpilibj.templates;
 
 public class RobotMap {
+
     public static final int LEFT_TALON_1 = 0;
     public static final int LEFT_TALON_2 = 0;
     public static final int RIGHT_TALON_1 = 0;
     public static final int RIGHT_TALON_2 = 0;
     public static final int JOYSTICK = 0;
-    public static final int JACK_RELAY_1=0;
+    public static final int JACK_RELAY_1 = 0;
     public static final int JACK_REALY_2 = 0;
     public static final int JACK_DOOR = 0;
-    public static final int JACK_DI_UP=0;
-    public static final int JACK_DI_DOWN=0;
-    public static final int JACK_DI_OPEN=0;
-    public static final int JACK_DI_CLOSED=0;
-    public static final int 
-    public static final int 
-            
-            }
+    public static final int JACK_DI_UP = 0;
+    public static final int JACK_DI_DOWN = 0;
+    public static final int JACK_DI_OPEN = 0;
+    public static final int JACK_DI_CLOSED = 0;
+    public static final int ARM_PORT = 0;
+    public static final int ARM_UP = 0;
+    public static final int ARM_DOWM = 0;
+    public static final int ROLLER_PORT1 = 0;
+    public static final int ROLLER_PORT2 = 0;
+    public static final int ROLLER_PORT3 = 0;
+    public static final int ROLLER_PORT4 = 0;
+}
